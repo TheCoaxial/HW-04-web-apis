63eba41fd79e070f7e3c1048276c8607f2cdc88e
Coaxial <thecoaxial@gmail.com>
2020-11-07 14:33:41 -0500

    rethinking my approach, added HTML elements and deleted irrelevant JS

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
 // Global variables
 var quizQuestions = ["this is the first question", "this is the second question", "this is the third question", "this is the fourth question"];
-var quizAnswers = ["filler1", "filler2"];
+var quizAnswersOne = ["filler1", "filler2","filler3","filler4"];
+var quizAnswersTwo = ["filler1", "filler2","filler3","filler4"];
+var quizAnswersThree = ["filler1", "filler2","filler3","filler4"];
+var quizAnswersFour = ["filler1", "filler2","filler3","filler4"];
 var totalSeconds = 75;
 var secondsElapsed = 0;
 var status = "start";
@@ -25,58 +28,9 @@ startButton.addEventListener("click", function(){
     questionGroup = 1;
     
     startTimer();
-    questionBuilder();
-    buttonBuilder();
-    
-    var theQuestion = document.getElementById("headingQuestion");
-    var buttonOne = document.getElementById("firstbutton");
-    var buttonTwo = document.getElementById("secondbutton");
-    var buttonThree = document.getElementById("thirdbutton");
-    var buttonFour = document.getElementById("fourthbutton");
-
-
-    //Creating a function to remove the buttons
-    function buttonRemover(){
-    buttonOne.remove();
-    buttonTwo.remove();
-    buttonThree.remove();
-    buttonFour.remove();
-    }
-
-    for(var i = 0; i < quizAnswers.length; i++){
-        buttonBuilder();
-        buttonOne.innerText = quizAnswers[i];
-        buttonTwo.innerHTML = quizAnswers[i];
-        buttonThree.innerHTML = quizAnswers[i];
-        buttonFour.innerHTML = quizAnswers[i];
-    }
-    
+      
 });
 
-
-
-// Creating a function to create 4 buttons for answers
-function buttonBuilder(){
-    var firstAnswer = document.createElement("button");
-    firstAnswer.setAttribute("id", "firstbutton");
-    body.appendChild(firstAnswer);
-    document.body.appendChild(document.createElement("br"));
-    
-    var secondAnswer = document.createElement("button");
-    secondAnswer.setAttribute("id", "secondbutton");
-    body.appendChild(secondAnswer);
-    document.body.appendChild(document.createElement("br"));
-
-    var thirdAnswer = document.createElement("button");
-    thirdAnswer.setAttribute("id", "thirdbutton");
-    body.appendChild(thirdAnswer);
-    document.body.appendChild(document.createElement("br"));
-
-    var fourthAnswer = document.createElement("button");
-    fourthAnswer.setAttribute("id", "fourthbutton");
-    body.appendChild(fourthAnswer);
-    
-}
 
 
 
